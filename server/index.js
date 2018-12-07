7daeff58fb09ab2ff1d5ce2e3e31f8266f77687e
Alex <aberman12@yahoo.com>
2018-12-06 20:47:12 -0800

    finished

--- server/index.js
+++ server/index.js
@@ -1,29 +1,37 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-// UNCOMMENT THE DATABASE YOU'D LIKE TO USE
-// var items = require('../database-mysql');
-// var items = require('../database-mongo');
-
+var express = require("express");
+var bodyParser = require("body-parser");
+var turf = require("@turf/turf");
+var JSON = require("./dma-polygons.json");
 var app = express();
+let didFind = false;
 
-// UNCOMMENT FOR REACT
-// app.use(express.static(__dirname + '/../react-client/dist'));
-
-// UNCOMMENT FOR ANGULAR
-// app.use(express.static(__dirname + '/../angular-client'));
-// app.use(express.static(__dirname + '/../node_modules'));
-
-app.get('/items', function (req, res) {
-  items.selectAll(function(err, data) {
-    if(err) {
-      res.sendStatus(500);
-    } else {
-      res.json(data);
+app.get("/findDMA", function(req, res) {
+  if (req.query.lat && req.query.lng) {
+    var long = Number(req.query.lng);
+    var lat = Number(req.query.lat);
+    var points = turf.points([[long, lat]]);
+    for (var i = 1; i < JSON.features.length; i++) {
+      var newJSONToCheck = JSON.features[i].geometry.coordinates;
+      var searchWithin = turf.multiPolygon(newJSONToCheck);
+      var doesLandInDMK = turf.pointsWithinPolygon(points, searchWithin);
+      if (
+        doesLandInDMK.features.length &&
+        doesLandInDMK.features[0].geometry.type &&
+        doesLandInDMK.features[0].geometry.type === "Point"
+      ) {
+        didFind = true;
+        res.send({ DMA: JSON.features[i].properties.dma_code });
+        break;
+      }
     }
-  });
+    if (!didFind) {
+      res.sendStatus(400).send("No DMA Match");
+    }
+  } else {
+    res.sendStatus(400).send("error: paramater missing");
+  }
 });
 
 app.listen(3000, function() {
-  console.log('listening on port 3000!');
+  console.log("listening on port 3000!");
 });
-
